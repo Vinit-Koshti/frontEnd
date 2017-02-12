d5023b4e3f964fbf66989279e14997ad719655b9
siddharthshashikar <siddharth.shashikar@st.niituniversity.in>
2017-02-12 17:27:04 +0530

    external js linking

---
+++ Shaguan WrappersZ/script.js
@@ -0,0 +1,9 @@
+ function change(){
+    document.getElementById("name").innerHTML = "$h@...@n W.@pp+rz";
+    document.getElementById("name").style.marginLeft="0";
+}
+
+function back(){
+    document.getElementById("name").innerHTML = "SHAGUAN WRAPPERSZ";
+    document.getElementById("name").style.marginLeft="0";
+}
